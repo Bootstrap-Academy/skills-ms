bf11a9d62c1825f06b100cfd6b200bbb89f37457
Defelo <elodef42@gmail.com>
2022-09-05 17:31:03 +0200

    Remove test endpoints

--- api/endpoints/__init__.py
+++ api/endpoints/__init__.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
-from . import skill, test
+from . import skill
 
 
 ROUTERS: dict[str, tuple[APIRouter, str | None]] = {
-    module.router.tags[0]: (module.router, module.__doc__) for module in [skill, test]
+    module.router.tags[0]: (module.router, module.__doc__) for module in [skill]
 }
